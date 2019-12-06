f9ddf45e3d164c5534d31f788505048783e53dda
moteinstorm <454665830@qq.com>
2019-12-06 11:23:54 +0800

    完成

--- src/main/java/com/zhuzhiguang/cms/utils/DateUtils.java
+++ src/main/java/com/zhuzhiguang/cms/utils/DateUtils.java
@@ -3,6 +3,7 @@ package com.zhuzhiguang.cms.utils;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * 关于日期的工具类
@@ -91,7 +92,71 @@ public class DateUtils {
 		instance.set(Calendar.DATE, 1);// 设置1日
 		
 		return instance.getTime();
+	}
+	
+	/**
+	 * 获取当前月的月末
+	 * @return
+	 */
+	public static Date getEndOfMonth() {
+		// 获取日历的实例
+		Calendar instance = Calendar.getInstance();
+		// 设置成当前的时间
+		instance.setTime(new Date());
+		instance.add(Calendar.MONTH, 1);// 月份加1
+		
+		// 下列代码设置成月初
+		instance.set(Calendar.SECOND, 0);// 设置0秒
+		instance.set(Calendar.MINUTE, 0);// 设置0分
+		instance.set(Calendar.HOUR, 0);// 设置0小时
+		instance.set(Calendar.AM_PM, Calendar.AM);// 设置上午
+		instance.set(Calendar.DATE, 1);// 设置1日
+		
+		// 减去一秒 变成当月的月末
+		instance.add(Calendar.SECOND, -1);// 秒减去1
+		return instance.getTime();
 		
 	}
+	
+	/**
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public static boolean  isThisWeek(Date date) {
+
+		SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+
+		Calendar firstDayOfWeek = Calendar.getInstance(Locale.getDefault());
+
+		firstDayOfWeek.setFirstDayOfWeek(Calendar.MONDAY);
+
+		int day = firstDayOfWeek.get(Calendar.DAY_OF_WEEK);
+
+		firstDayOfWeek.add(Calendar.DATE, -day+1+1);// 后面的+1是因为从周日开始
+
+		// 本周一的日期
+
+		System.out.println(format.format(firstDayOfWeek.getTime()));
+
+		Calendar lastDayOfWeek = Calendar.getInstance(Locale.getDefault());
+
+		lastDayOfWeek.setFirstDayOfWeek(Calendar.MONDAY);
+
+		day = lastDayOfWeek.get(Calendar.DAY_OF_WEEK);
+
+		lastDayOfWeek.add(Calendar.DATE, 7-day+1);
+
+		// 本周星期天的日期
+
+		System.out.println(format.format(lastDayOfWeek.getTime()));
+		
+		return (date.getTime()<lastDayOfWeek.getTime().getTime() &&
+				date.getTime()>firstDayOfWeek.getTime().getTime() );
+
+	}
+	
+	
+	
 	
 }
